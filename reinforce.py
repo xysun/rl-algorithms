75b14f011fc498162825d827c32bfaed0bfb0f7c
Xiayun Sun <xiayunsun@gmail.com>
2018-12-24 13:25:24 +0000

    refactor

--- reinforce.py
+++ reinforce.py
@@ -11,35 +11,50 @@ import random
 import gym
 import numpy as np
 import tensorflow as tf
+from sklearn.preprocessing import OneHotEncoder
 
 tf.enable_eager_execution()
 
 env = gym.make('CartPole-v1')
+state_n = env.observation_space.shape[0]
+action_n = env.action_space.n
+
+alpha = 0.0005  # learning rate
+discount = 1
+
+solved_lookback = 100
+solved_threwhold = 295.0
 
 
 def train(episodes):
-    weights = tf.convert_to_tensor(np.random.rand(4, 2))
+    weights = tf.convert_to_tensor(np.random.rand(state_n, action_n))
+    encoder = OneHotEncoder()
+    encoder.fit([[e] for e in [i for i in range(action_n)]])
+
+    scores = []
+
+    def update(observation):
+        # so many global variables it hurts :(
+        with tf.GradientTape() as t:
+            t.watch(weights)
+            a = tf.reshape(tf.convert_to_tensor(np.array(observation)), (1, state_n))
+            z = tf.matmul(a, weights)
+            p = tf.nn.softmax(z)
+            action = random.choices(range(env.action_space.n), weights=p.numpy().reshape(action_n, ), k=1)[0]
+            action_ohc = np.array(encoder.transform([[action]]).toarray(), dtype='float64')
+            target = tf.multiply(tf.reshape(p, (action_n,)), tf.reshape(action_ohc, (action_n,)))
+            lp = tf.log(tf.reduce_sum(target))
+        return action, lp, t
 
     for i in range(episodes):
         observation = env.reset()
         rewards = []
-        pis = []
         observations = []
         gradients = []
 
         # first action
         observations.append(observation)
-
-        with tf.GradientTape() as t:
-            t.watch(weights)
-            a = tf.reshape(tf.convert_to_tensor(np.array(observation)), (1,4))
-            z = tf.matmul(a,weights)
-            p = tf.nn.softmax(z)
-            action = random.choices([0, 1], weights=p.numpy().reshape(2, ), k=1)[0]
-            action_ohc = tf.constant([1.0,0.0], dtype='float64') if action == 0 else tf.constant([0.0,1.0], dtype='float64')
-            target = tf.multiply(tf.reshape(p, (2,)), tf.reshape(action_ohc, (2,)))
-            lp = tf.log(tf.reduce_sum(target))
-
+        action, lp, t = update(observation)
         gradient = t.gradient(lp, weights).numpy()
         gradients.append(gradient)
         observation, reward, is_done, info = env.step(action=action)
@@ -47,35 +62,44 @@ def train(episodes):
 
         while not is_done:
             observations.append(observation)
-
-            with tf.GradientTape() as t:
-                t.watch(weights)
-                a = tf.reshape(tf.convert_to_tensor(np.array(observation)), (1, 4))
-                z = tf.matmul(a, weights)
-                p = tf.nn.softmax(z)
-                action = random.choices([0, 1], weights=p.numpy().reshape(2, ), k=1)[0]
-                action_ohc = tf.constant([1.0, 0.0], dtype='float64') if action == 0 else tf.constant([0.0, 1.0],
-                                                                                                      dtype='float64')
-                target = tf.multiply(tf.reshape(p, (2,)), tf.reshape(action_ohc, (2,)))
-                lp = tf.log(tf.reduce_sum(target))
-
+            action, lp, t = update(observation)
             gradient = t.gradient(lp, weights).numpy()
             gradients.append(gradient)
             observation, reward, is_done, info = env.step(action=action)
             rewards.append(reward)
-
-        alpha = 0.001  # learning rate
-        discount = 1
 
         for j in range(0, len(rewards)):
             g = sum(rewards[j:])
             gradient = gradients[j]
             weights += alpha * g * pow(discount, j) * gradient
 
-        # if i % 1000 == 0:
+        with open('logs/reinforce_cartpole.csv', 'a') as f:
+            f.write("%d,%d\n" % (i, sum(rewards)))
         print("episode %d, rewards %d" % (i, sum(rewards)))
+
+        scores.append(sum(rewards))
+        if len(scores) > solved_lookback:
+            # check whether solved or not
+            if sum(scores[-solved_lookback:]) / float(solved_lookback) >= solved_threwhold:
+                print("Solved at episode ", i)
+                break
+
+    return weights
 
 
 if __name__ == '__main__':
-    episodes = 1000000
-    train(episodes)
+    episodes = 5000
+    weights = train(episodes)
+
+    # demonstrate
+    observation = env.reset()
+    for _ in range(1000):
+        env.render()
+        a = tf.reshape(tf.convert_to_tensor(np.array(observation)), (1, state_n))
+        z = tf.matmul(a, weights)
+        p = tf.nn.softmax(z)
+        action = random.choices(range(env.action_space.n), weights=p.numpy().reshape(action_n, ), k=1)[0]
+        next_observation, reward, is_done, info = env.step(action)
+        observation = next_observation
+        if is_done:
+            break
